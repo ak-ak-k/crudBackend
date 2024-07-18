b0f9b15e2163d793b516bbfe90e5ece184c0ca73
aditya <adityakirtane27@gmail.com>
2024-07-18 13:24:32 +0530

    sc

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
 const mongoose = require("mongoose");
 
-mongoose
-  .connect(
-    "mongodb+srv://doadmin:523fQKF96aZ0n1c7@db-mongodb-blr1-55155-1cb7a79a.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=db-mongodb-blr1-55155"
-    // {
-    //   useNewUrlParser: true,
-    //   useUnifiedTopology: true,
-    // }
-  )
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.error("Error connecting to MongoDB:", err);
-  });
+const connectWithRetry = () => {
+  console.log("MongoDB connection with retry");
+  mongoose
+    .connect(
+      "mongodb+srv://doadmin:523fQKF96aZ0n1c7@db-mongodb-blr1-55155-1cb7a79a.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=db-mongodb-blr1-55155"
+      // {
+      //   useNewUrlParser: true,
+      //   useUnifiedTopology: true,
+      // }
+    )
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+      console.error(
+        "Error connecting to MongoDB, retrying in 5 seconds...",
+        err
+      );
+      setTimeout(connectWithRetry, 5000);
+    });
+};
+
+connectWithRetry();
 
 module.exports = mongoose;
